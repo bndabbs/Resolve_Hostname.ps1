1f1c722366e5839683564b09d3baedb3f185e8bd
bndabbs <bndabbs@gmail.com>
2015-10-02 13:04:26 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Synopsis
+Resolves a list of hostnames to IP addresses 
+
+# Description
+This script calls a .Net function to do the DNS resolution, since there isn't a good way to do this built into Powershell. 
+
+# Notes
+Be sure to update the file name on line 1 to match your environment.
---
+++ Resolve_Hostname.ps1
@@ -0,0 +1,14 @@
+﻿$ServerList = Import-Csv -Path C:\Temp\servers.csv -Header Name
+$array = $null
+$array = @()
+
+Foreach ($Server in $ServerList) {
+    $Address = [System.Net.Dns]::GetHostAddresses($Server.Name)
+	$obj = New-Object PSObject
+		$obj | Add-Member -MemberType NoteProperty -Name "Computer Name" -Value $Server.Name
+		$obj | Add-Member -MemberType NoteProperty -Name "Computer Address" -Value $Address.IPAddressToString
+	$array += $obj
+	$Address = $null
+	}
+
+$array | Export-Csv -Path "C:\Temp\resolved.csv" -NoTypeInformation
